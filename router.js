23f34b3ef66c81eca7726534e48fee252028ee6f
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-21 05:08:13 +0000

    added cors

--- router.js
+++ router.js
@@ -1,12 +1,13 @@
+const cors = require('cors');
 var express = require('express'),
     router = express.Router();
 
-    router.get('/', (req, res) => {
+    router.get('/', cors(), (req, res) => {
       res.send('Hello World!')
     })
 
-    router.post('/test', (req, res) => {
-      res.send('Hello test')
+    router.post('/createroom', cors(), (req, res) => {
+      res.send('abcd')
     })
 
 module.exports = router;
