19cd412050fa755fb707f17c50f145192ff058ca
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-22 05:02:02 +0000

    allowed passing settings to createroom

--- channel.js
+++ channel.js
@@ -1,16 +1,14 @@
 class Channel
 {
-    constructor() {
-        //
-    }
+	constructor() {
+		//
+	}
 
-    static createRoom() {
-        return (
-            Math.floor(Math.random() * (16777215 - 1048576) + 1048576)
-            .toString(16)
-            .toUpperCase()
-        );
-    }
+	static createRoom(body) {
+		let roomID = Math.floor(Math.random() * (16777215 - 1048576) + 1048576).toString(16).toUpperCase()
+		//save the room in the database with the settings
+		return (roomID);
+	}
 }
 
 module.exports = Channel;
--- router.js
+++ router.js
@@ -3,13 +3,15 @@ const Channel = require('./channel');
 const cors = require('cors');
 var express = require('express'),
   router = express.Router();
+  router.use(cors());
+  router.use(express.json())
 
-  router.get('/', cors(), (req, res) => {
+  router.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
   router.post('/createroom', cors(), (req, res) => {
-    res.send(Channel.createRoom())
+    res.send(Channel.createRoom(req.body))
   })
 
 module.exports = router;
